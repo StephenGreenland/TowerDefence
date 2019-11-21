9dda3b9a1c7c26f2a64dee1fbecd3085556e1097
Stephen Greenland <1013573@student.sae.edu.au>
2019-11-21 10:55:41 +0800

    peaches and cream

---
+++ Rending/Assets/Renderer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using System.Runtime.InteropServices;
+ using UnityEngine;
+ 
+ namespace Stephen
+ {
+ 
+ 
+ 
+     public class Renderer : MonoBehaviour
+     {
+         private byte[,] backBuffer;
+         public Texture quadRenderer;
+ 
+         // Start is called before the first frame update
+         void Start()
+         {
+             backBuffer = new byte[16,16];
+             
+             Texture2D tex = new Texture2D(16, 16, TextureFormat.RGB24, false);
+             tex.filterMode = FilterMode.Point; // This turns off blur for debugging
+           //  quadRenderer.Material.mainTexture = tex;
+ 
+ 
+         }
+ 
+         // Update is called once per frame
+         void Update()
+         {
+ 
+         }
+     }
+ }
--- TowerFence/Assets/BasicUnit.cs
+++ TowerFence/Assets/BasicUnit.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class BasicUnit : MonoBehaviour
 {
-    public List<Vector2> path;
+    private List<Vector2> path;
     public scanner Grid;
 
     private int count;
+
+    private float timer;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        count = 0;
+        path = Grid.finalPath;
+        timer = 1;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        timer -= Time.deltaTime;
+        
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(path[count].x, 0, path[count].y), 1f);
-        count++;
+
+        if (timer < 0)
+        {
+            timer = .5f;
+            count++;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(path[count].x, 0, path[count].y), 1f);
+
+        }
+        
 
 
     }
